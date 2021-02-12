b4bfe3a8597aae34658267c3056af55ae6b03a45
Manoel Luis <manoelluisjunior@hotmail.com>
2021-02-12 14:25:27 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/manoeljunior/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoeljunior.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
