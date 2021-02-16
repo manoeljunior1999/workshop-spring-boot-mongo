37aba0fd09ef9e18eadd4cdbb5a73bc115a9ab39
Manoel Luis <manoelluisjunior@hotmail.com>
2021-02-16 14:30:37 -0300

    Consulta com vários critérios

--- src/main/java/com/manoeljunior/workshopmongo/resources/util/URL.java
+++ src/main/java/com/manoeljunior/workshopmongo/resources/util/URL.java
@@ -2,6 +2,9 @@ package com.manoeljunior.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	
@@ -12,5 +15,17 @@ public class URL {
 			return "";
 		}
 	}
-
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (java.text.ParseException e) {
+			return defaultValue;
+		}
+	}
 }
+
+
+
